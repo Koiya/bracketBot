2fad42ede2a5c7822f29dad72607fcccad37b5cd
Daniel Nguyen <zeromsv@gmail.com>
2024-07-22 10:50:57 -0500

    refactor

--- bot/newMessage.go
+++ bot/newMessage.go
@@ -1,43 +1,10 @@
-package bot
+package newMessage
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
-
-var BotToken string
-
-func checkNilErr(e error) {
-	if e != nil {
-		log.Fatal("Error message")
-	}
-}
-
-func Run() {
-
-	// create a session
-	discord, err := discordgo.New("Bot " + BotToken)
-	checkNilErr(err)
-
-	// add a event handler
-	discord.AddHandler(newMessage)
-
-	// open session
-	discord.Open()
-	defer discord.Close() // close session, after function termination
-
-	// keep bot running untill there is NO os interruption (ctrl + C)
-	fmt.Println("Bot running....")
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
-
-}
 
 func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
 
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/pelletier/go-toml"
+	"os"
+	"os/signal"
 )
 
 func main() {
@@ -18,10 +20,18 @@ func main() {
 		fmt.Println("Error creating Discord session,", err.Error())
 		return
 	}
-
+	// add a event handler
+	dg.AddHandler(newMessage)
 	if err := dg.Open(); err != nil {
 		fmt.Println("Error opening connection,", err.Error())
 		return
 	}
 	defer dg.Close()
+
+	// keep bot running untill there is NO os interruption (ctrl + C)
+	fmt.Println("Bot running....")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
 }
